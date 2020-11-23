3242ea2ecac6f2a0179a9ea8b6c94d24adef0f51
BrunoHBG <bruno.homberg@live.fr>
2020-11-23 10:48:45 +0100

    Update table user

--- db/migrate/20201123092118_devise_create_users.rb
+++ db/migrate/20201123092118_devise_create_users.rb
@@ -3,6 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+
+      t.belongs_to :carts
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
