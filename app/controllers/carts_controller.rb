cca94392ebb217e6f13ab61dddcbfc20d4962ebc
Pierre Henry <pierre.henry21@outlook.fr>
2020-11-24 14:29:04 +0100

    View welcome email

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,7 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_current_user, only: [:show]
+
   def index
     @carts = Cart.all
   end
@@ -9,6 +12,22 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find_by(user_id: params[:id])
+  end
+
+  def update
+    @cart = Cart.find_by(user_id: current_user.id)
+    
+
+    #Ajouter le JS
+  end
+
+  def destroy
+    @user = current_user
+    @item = Item.find_by(id: params[:id])
+    @cart = Cart.find_by(user_id: current_user.id)
+    
+
+    #Ajouter le JS
   end
 
 end
