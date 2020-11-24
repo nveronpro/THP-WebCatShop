d81d3562ae044099df8c255a85a61f4e64098c78
Pierre Henry <pierre.henry21@outlook.fr>
2020-11-24 16:19:29 +0100

    Modif view show product

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,5 @@
 class CartsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_current_user, only: [:show]
 
   def index
     @carts = Cart.all
@@ -8,22 +7,6 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find_by(user_id: params[:id])
-  end
-
-  def update
-    @cart = Cart.find_by(user_id: current_user.id)
-    
-
-    #Ajouter le JS
-  end
-
-  def destroy
-    @user = current_user
-    @item = Item.find_by(id: params[:id])
-    @cart = Cart.find_by(user_id: current_user.id)
-    
-
-    #Ajouter le JS
   end
 
 end
