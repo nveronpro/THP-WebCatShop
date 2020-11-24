098ee122bf05b68917d47405ad56587f2cdaeada
Nico <arrvac@gmail.com>
2020-11-24 15:25:37 +0100

    last name and first name

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2020_11_23_105301) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.bigint "cart_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
