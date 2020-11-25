3af8f0b1950288e8db729525e2bbd401de36d5eb
mmolinar <mariamolinaryarza@gmail.com>
2020-11-25 17:34:00 +0100

    Rename routes part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users
+  resources :users, path: :mon_compte
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :items
-  resources :carts
+  resources :items, path: :jouets
+  
+  resources :carts, path: :panier
   resources :carditems
   resources :charges
   post '/carditems/:id', to: 'carditems#create', as: 'panier'
