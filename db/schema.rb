081120934030b87d4d4b4aa8d4d188e30f9946f8
Pierre Henry <pierre.henry21@outlook.fr>
2020-11-26 10:44:08 +0100

    Adding Category to Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_154423) do
+ActiveRecord::Schema.define(version: 2020_11_26_094224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_154423) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   create_table "orders", force: :cascade do |t|
